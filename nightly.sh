9d753471f72db1c9a32fe4645d2d177f88ad6603
adnanhd <adnanharundogan@gmail.com>
2026-07-07 12:37:24 +0300

    build: migrate to pyproject, drop setup.py and tox

--- nightly.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-repository="$(basename "$(git rev-parse --show-toplevel)")"
-# Version equal to current timestamp
-echo "__version__ = \"$(date +%s)\"" >"$repository"/_version.py
-echo "_name = \"$repository-nightly\"" >"$repository"/_name.py
